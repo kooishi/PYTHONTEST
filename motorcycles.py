046556392de4797be2d6fc68fc4f455f330bf376
koishi <Ms.Koishi@outlook.com>
2025-03-16 17:04:01 +0800

    接下来练习

--- motorcycles.py
+++ motorcycles.py
@@ -7,11 +7,35 @@
 # motorcycles.append('ducati')
 # print(motorcycles)
 
-motorcycles = []
-motorcycles.append('honda')
-motorcycles.append('yamaha')
-motorcycles.append('suzuki')
+# motorcycles = []
+# motorcycles.append('honda')
+# motorcycles.append('yamaha')
+# motorcycles.append('suzuki')
 
 # print(motorcycles)
 
-motorcycles.insert(0, 'ducati')
+# motorcycles.insert(0, 'ducati')
+# print(motorcycles)
+
+# del motorcycles[1]
+# print(motorcycles)
+
+# popped_motorcycles = motorcycles.pop()
+# print(popped_motorcycles)
+# print(motorcycles)
+
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+print(motorcycles)
+
+# popped_motorcycle = motorcycles.pop()
+# print(motorcycles)
+# print(popped_motorcycle)
+
+# first_owned = motorcycles.pop(0)
+# print("The first motorcycle I owned was a " + first_owned.title() + ".")
+
+
+too_expensive = 'ducati'
+motorcycles.remove(too_expensive)
+print(motorcycles)
+print("\nA " + too_expensive.title() + " is too expensive for me.")
