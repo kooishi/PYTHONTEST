958750aea195d20dd587cb80ee4a9097ca0cab3b
koishi <Ms.Koishi@outlook.com>
2025-03-16 17:23:10 +0800

    3.3

---
+++ test.py
@@ -0,0 +1,48 @@
+# 3-4
+
+# List = ['wang', 'qian', 'li', 'zhao']
+# print(List)
+
+# 3-5
+# print(List[1].title() + " is cannot come to dinner.")
+# List[1] = 'sun'
+# print(List)
+
+# 3-6
+# print("I found a bigger table.")
+
+# List.insert(0, 'zhou')
+# List.insert(2, 'wu')
+# List.append('zheng')
+# print(List)
+
+# 3-7
+# print("I can only invite two people to dinner.")
+# print("Sorry, " + List.pop() + ", I can't invite you to dinner.")
+# print("Sorry, " + List.pop() + ", I can't invite you to dinner.")
+# print("Sorry, " + List.pop() + ", I can't invite you to dinner.")
+# print("Sorry, " + List.pop() + ", I can't invite you to dinner.")
+# print("Sorry, " + List.pop() + ", I can't invite you to dinner.")
+# print(List)
+# print("You are still invited to dinner, " + List[0] + ".")
+# print("You are still invited to dinner, " + List[1] + ".")
+
+
+# 3-8
+travel = ['beijing', 'shanghai', 'guangzhou', 'shenzhen', 'chengdu']
+print(travel)
+print(sorted(travel))
+print(travel)
+print(sorted(travel, reverse=True))
+print(travel)
+travel.reverse()
+print(travel)
+travel.reverse()
+print(travel)
+travel.sort()
+print(travel)
+travel.sort(reverse=True)
+print(travel)
+
+# 3-9
+print(len(travel))
